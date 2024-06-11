72d8bdda1000bfcaf8c26f6e5429e906521d0744
kishhhore <kishking2004@gmail.com>
2024-06-11 23:27:47 +0530

    main file

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,6 @@
+/*import beeper from 'beeper';
+await beeper();
+*/
+//const superhero = require("superheroes");
+import  { randomSuperhero } from "superheroes";
+console.log(`I am a ${randomSuperhero()}`);
